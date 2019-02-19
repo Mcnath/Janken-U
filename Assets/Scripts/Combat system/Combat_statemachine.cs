21d2dc62072297852207e270be0819e6e9589696
Mcnath <mcnath45@gmail.com>
2019-02-20 00:27:28 +0800

    Temp_combat test

---
+++ Assets/Scripts/Combat system/Combat_statemachine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Combat_statemachine : MonoBehaviour {
+
+	public enum battleState
+	{
+		START,
+		PLAYERCHOICE,
+		ENEMYCHOICE,
+		LOSE,
+		WIN
+	}
+
+	private battleState currentState;
+
+
+	// Use this for initialization
+	void Start () {
+		currentState = battleState.START;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (currentState);
+		switch(currentState){
+		case(battleState.START):
+			//setup battle function
+			break;
+		case(battleState.PLAYERCHOICE):
+			break;
+		case(battleState.ENEMYCHOICE):
+			break;
+		case(battleState.LOSE):
+			break;
+		case(battleState.WIN):
+			break;
+		}
+	}
+	void onGUI(){
+		if (GUILayout.Button ("NEXT STATE")) {
+			{
+				currentState = (battleState)(((int)currentState + 1) % 5);
+			}
+		}
+	}
+}
+
---
+++ Assets/Scripts/Player Classes/Freshmores.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freshmores {
+
+	private string CharacterPillarName;
+	private string PillarDescription;
+
+	//stat
+	private int priority;
+	private int endurance;
+	private int range;
+
+	public string chara_pillarName {
+		get{ return CharacterPillarName; }
+		set{ CharacterPillarName = value; }
+	}
+
+	public string chara_describePillar {
+		get{ return PillarDescription; }
+		set{ PillarDescription = value; }
+	}
+
+	public int chara_priority {
+		get{ return priority; }
+		set{ priority = 0; }
+	}
+
+	public int chara_endurance {
+		get{ return endurance; }
+		set{ endurance = 0; }
+	}
+
+	public int chara_range {
+		get{ return range; }
+		set{ range = 0; }
+	}
+
+
+}
